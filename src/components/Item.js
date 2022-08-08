fcd2dca25fcb7290393a5509284ed672fe879d20
Chris Luong <christopher_luong@live.ca>
2022-08-08 17:33:55 -0400

    add form with select option for quantity

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,16 @@
+import '../styles/shop.css'
+
 function Item(props) {
   let {title, price, src, alt} = props
+  
+  const generateQuantity = () => {
+    let options = []
+    for(let index = 1; index <= 10; index++) {    
+      options.push(<option value={index}>{index}</option>)
+      
+    }
+    return options;
+  }
   return (
     <div className="item">
       <div>
@@ -11,6 +22,14 @@ function Item(props) {
       <div>
         <div>${price}</div>
       </div>
+      <form action="">
+        <label htmlFor="quantity">Quantity</label>
+        <select id="quantity">
+          {
+            generateQuantity()
+          }
+        </select>
+      </form>
     </div>
   );
 }
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,17 +3,12 @@ import Inventory from "../fixtures/inventory";
 import Item from "./Item";
 import '../styles/shop.css'
 
-function Shop() {
-    const imgFolder = require.context('../images', false)
-    
+function Shop() {        
     let getInventory = () => {
+      const imgFolder = require.context('../images', false)
       return Inventory.map(item => {
-        let title = item.title;
-        let price = item.price;
-        let alt = item.image.alt;
         const img = imgFolder(item.image.src)
-        // return (<img src={img} alt={item.image.alt} width="300px"/>)
-        return <Item title={title} price={price} src={img} alt={alt}></Item>
+        return <Item title={item.title} price={item.price} src={img} alt={item.image.alt}></Item>
       });
     }
     return (
